ab665598111a6ef90b6e88c08f3acc69fea0c784
wimanshaherath <wimanshah@gmail.com>
2020-12-12 00:24:58 +0530

    created footer component file

---
+++ src/compnents/footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Paper,Box,Typography} from '@material-ui/core';
+const Footer:React.FC=() => {
+    return(
+        <div></div>
+    );
+}
+
+export default Footer;
