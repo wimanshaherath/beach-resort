1b8383ad6e8d898a2fe7529f133668430c500a70
wimanshaherath <wimanshah@gmail.com>
2020-12-12 00:53:19 +0530

    dev/added basic background styles to footer

--- src/compnents/footer.tsx
+++ src/compnents/footer.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import {Paper,Box,Typography} from '@material-ui/core';
+import {makeStyles,createStyles,Theme} from '@material-ui/core';
+
+const useStyle=makeStyles((theme:Theme) =>
+    createStyles({
+        paperContainer:{
+            position:'relative',
+            bottom:'0',
+            minHeight:'6rem',
+            width:'100vw',
+            background:'rgb(55, 236, 186)'
+        }
+    })
+)
 const Footer:React.FC=() => {
+    const classes=useStyle();
     return(
-        <div></div>
+        <Paper className={classes.paperContainer}>
+            <Typography variant='h4' component='h4'>
+                footer
+            </Typography>
+        </Paper>
     );
 }
 
