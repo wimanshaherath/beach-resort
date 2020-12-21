9d3616c80c8cb04b3c98b1a87c72f30fc7228dad
wimanshaherath <wimanshah@gmail.com>
2020-12-21 15:47:57 +0530

    dev/completed Services component

--- src/compnents/Services.tsx
+++ src/compnents/Services.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
-import {Grid,Typography,Paper} from '@material-ui/core';
+import React, {useEffect, useState} from 'react';
+import {Grid,Typography,Paper,Card,CardActionArea,CardContent,CardMedia} from '@material-ui/core';
 import {makeStyles,createStyles,Theme} from '@material-ui/core';
-import classes from '*.module.css';
+import {FaCocktail,FaBeer,FaShuttleVan,FaWifi} from 'react-icons/fa';
+export type Service = {
+    icon:JSX.Element;
+    title:string;
+    description:string;
+}
 
 const useStyle = makeStyles((theme:Theme) =>
 createStyles({
@@ -18,24 +23,91 @@ createStyles({
         [theme.breakpoints.down('sm')]:{
             
         }
+    },
+    cardmedia:{
+        
+        fontSize:'4rem'
     }
 }))
+
+
 const Services: React.FC = () =>{
     const classes = useStyle();
+    const [services, setServices] = useState<Service[]>(
+        [
+            {
+                icon:<FaCocktail/>,
+                title:"free cocktails",
+                description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. A arcu cursus vitae congue mauris."
+            },
+            {
+                icon:<FaBeer/>,
+                title:"strong beer",
+                description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. A arcu cursus vitae congue mauris."
+            },
+            {
+                icon:<FaShuttleVan/>,
+                title:"free shuttle",
+                description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. A arcu cursus vitae congue mauris."
+            },
+            {
+                icon:<FaWifi/>,
+                title:"free wifi",
+                description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. A arcu cursus vitae congue mauris."
+            }
+        ]
+    );
+
+
     return(
         <Grid container>
-            <Grid item md={3} sm={6} xs={12}>
-                <Paper className={classes.paper}>xs3</Paper>
+            <Grid item xs={12}>
+                <Paper className={classes.paper}>
+                    <Typography variant="h4" component="h4">
+                        Services
+                    </Typography>
+                </Paper>
+            </Grid>
+            {services.map((service,index)=>{
+                return <Grid item md={3} sm={6} xs={12} key={index}>
+                    <Paper className={classes.paper}>
+                        <Card>
+                            <CardActionArea className={classes.cardmedia}>
+                                {service.icon}
+                            </CardActionArea>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h6">
+                                    {service.title}
+                                </Typography>
+                                <Typography variant='body2' component='p'>
+                                    {service.description}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Paper>
+                </Grid>
+                }
+            )}
+{/* 
+            {services.map((item,index) => {
+
+            })} */}
+            
+            {/* <Grid item md={3} sm={6} xs={12}>
+                <Paper className={classes.paper}>
+                    xs3
+                </Paper>
             </Grid>
             <Grid item md={3} sm={6} xs={12}>
-                <Paper className={classes.paper}>xs3</Paper>
+                <Paper className={classes.paper}>
+                    xs3
+                </Paper>
             </Grid>
             <Grid item md={3} sm={6} xs={12}>
-                <Paper className={classes.paper}>xs3</Paper>
-            </Grid>
-            <Grid item md={3} sm={6} xs={12}>
-                <Paper className={classes.paper}>xs3</Paper>
-            </Grid>
+                <Paper className={classes.paper}>
+                    xs3
+                </Paper>
+            </Grid> */}
         </Grid>        
     )
 }
