db94783ccfecef6ef5d688e1115aa1b2cdc047f9
wimanshaherath <wimanshah@gmail.com>
2021-01-13 23:04:23 +0530

    dev/completed RoomList component

--- src/compnents/RoomList.tsx
+++ src/compnents/RoomList.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Room from './Room';
-import {Grid} from '@material-ui/core';
-
+import {Grid,Typography} from '@material-ui/core';
+import {makeStyles,createStyles,Theme} from '@material-ui/core';
 type Irooms = {
     id:string;
     name:string;
@@ -21,11 +21,33 @@ type Irooms = {
 type Props= {
     rooms:Irooms[];
 }
+
+const useStyles = makeStyles((theme:Theme) =>
+    createStyles({
+        gridItem:{
+            magin:'.5rem',
+        }
+    })
+)
+
 const RoomList: React.FC<Props> = ({rooms}) =>{
+    const classes = useStyles();
+
+    if(rooms.length === 0){
+        return(
+            <>
+               <Grid container>
+                    <Typography>No matched room is found.</Typography>
+               </Grid>
+            </>
+        )
+    }
     return(
         <>
         <Grid container>
-            
+            {rooms.map((room) => {
+                return <Grid item xs = {12} sm={6} md={4}><Room room={room}/></Grid>
+            })}
         </Grid>
         </>
     )
